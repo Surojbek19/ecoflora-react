a7d8f33dfbcdbcf7bceafccfc05277ceb3297be9
Sam <surojbeksv@gmail.com>
2025-11-07 02:03:23 +0900

    feat: Initial Setup

---
+++ src/index.tsx
@@ -0,0 +1,21 @@
+import React from "react";
+import ReactDOM from "react-dom";
+import { Provider } from "react-redux";
+import { store } from "./app/store";
+import App from "./app/App";
+import reportWebVitals from "./reportWebVitals";
+import "./css/index.css";
+
+ReactDOM.render(  //.render() → actually draws your React components inside that root.
+  <React.StrictMode>
+    <Provider store={store}>
+      <App />
+    </Provider>
+  </React.StrictMode>,
+  document.getElementById("root") //document is REAL DOM's instance, and we are getting "root" from it's method
+);
+
+// If you want to start measuring performance in your app, pass a function
+// to log results (for example: reportWebVitals(console.log))
+// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
+reportWebVitals();
