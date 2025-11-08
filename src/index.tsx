517686eee3587f67c52eea317d1978713799fd2e
Sam <surojbeksv@gmail.com>
2025-11-09 02:33:06 +0900

    feat: create routing system

--- src/index.tsx
+++ src/index.tsx
@@ -7,6 +7,7 @@ import reportWebVitals from "./reportWebVitals";
 import  CssBaseline  from "@mui/material/CssBaseline";
 import { ThemeProvider } from "@mui/material/styles";
 import theme from "./app/material/MaterialTheme";
+import { BrowserRouter as Router } from "react-router-dom";
 import "./css/index.css";
 
 
@@ -15,7 +16,9 @@ ReactDOM.render(  //.render() → actually draws your React components inside th
     <Provider store={store}>
       <ThemeProvider theme={theme}>
          <CssBaseline />
-         <App />
+          <Router>
+            <App />
+          </Router>
       </ThemeProvider>
     </Provider>
   </React.StrictMode>,
