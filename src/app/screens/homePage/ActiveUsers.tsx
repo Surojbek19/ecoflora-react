67028e3c72efeb730e853e1581947b8168b996f0
Sam <surojbeksv@gmail.com>
2025-11-14 16:55:21 +0900

    feat: develop activeusers sectional components

--- src/app/screens/homePage/ActiveUsers.tsx
+++ src/app/screens/homePage/ActiveUsers.tsx
@@ -1,10 +1,52 @@
 import React from "react";
-import { Container } from "@mui/material";
+import { Box, Container, Stack } from "@mui/material";
+import Divider from "@mui/joy/Divider";
+import CardContent from "@mui/joy/CardContent";
+import Card from "@mui/joy/Card";
+import { CssVarsProvider, Typography } from "@mui/joy";
+import CardOverflow from "@mui/joy/CardOverflow";
+import AspectRatio from "@mui/joy/AspectRatio";
 
-export default function ActiveUsers() {
-    return (
-    <div>
-        <Container>ActiveUsers</Container>
+
+const activeUsers = [
+  {memberNick: "Martin", memberImage: "/img/martin.webp"},
+  {memberNick: "Justin", memberImage: "/img/justin.webp"},
+  {memberNick: "Rose", memberImage: "/img/rose.webp"},
+  {memberNick: "Nusret", memberImage: "/img/nusret.webp"}
+]
+
+export default function ActiveUsers(){
+  return (
+    <div className={"active-users-frame"}>
+      <Container>
+        <Stack className={"main"}>
+          <Box className={"category-title"}>Active Users</Box>
+          <Stack className={"cards-frame"}>
+            <CssVarsProvider>
+
+              {activeUsers.length !== 0 ? (
+                activeUsers.map((ele, index) => {
+                  return (
+                    <Card className={"card"} variant="outlined">
+                    <CardOverflow className="member-image">
+                      <AspectRatio ratio="1">
+                        <img src={ele.memberImage} alt=""/>
+                      </AspectRatio>
+                    </CardOverflow>
+                    
+                    <CardContent className={"card-content"}>
+                      <Typography className={"member-name"}>{ele.memberNick}</Typography>
+                    </CardContent>
+                  </Card>
+                    )
+                  })
+                 ) : (
+                    <Box className="no-data">No Active Users!</Box> 
+                  ) }
+            </CssVarsProvider>
+          </Stack>
+        </Stack>
+      </Container>
     </div>
-    )
+  )
 }
