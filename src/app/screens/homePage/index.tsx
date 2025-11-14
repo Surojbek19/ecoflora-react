3ac4fad92cebaafe70ac2cf038c2ff7d81381090
Sam <surojbeksv@gmail.com>
2025-11-14 13:45:01 +0900

    fix: modify statistics sectional component

--- src/app/screens/homePage/index.tsx
+++ src/app/screens/homePage/index.tsx
@@ -5,6 +5,7 @@ import NewDishes from "./NewDishes";
 import Advertisement from "./Advertisement";
 import Events from "./Events";
 import ActiveUsers from "./ActiveUsers";
+import "../../../css/home.css"
 
 export default function HomePage() {
   return <div className={"homepage"}>
