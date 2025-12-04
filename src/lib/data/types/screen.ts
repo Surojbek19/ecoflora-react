18a46aa3b1a320a5fc757bd90444e075634ec92c
Sam <surojbeksv@gmail.com>
2025-12-04 22:40:25 +0900

    feat: develop AppRootState and Homepage type integartions

---
+++ src/lib/data/types/screen.ts
@@ -0,0 +1,21 @@
+import { Member } from "./member";
+import { Product } from "./product";
+
+//screen components based type integrations
+/** RAECT APP STATE **/
+export interface AppRootState {
+    homePage: HomePageState; // homePage screen component
+    // productsPage: ProductsPageState;  // productsPage screen component
+    // ordersPage: OrdersPAgeState; // ordersPage screen component
+}
+
+/** HOME PAGE **/
+export interface HomePageState {
+ popularDishes: Product[];
+ newDishes: Product[];
+ topUsers: Member[]   
+}
+
+/** PRODUCTS PAGE **/
+
+/** ORDERS PAGE **/
