af25300cf8b94c3b6dc96a7dcf5c1002483e3f16
Sam <surojbeksv@gmail.com>
2026-05-14 02:13:22 +0900

    feat: develop Socket.io Client

--- src/index.tsx
+++ src/index.tsx
@@ -1,35 +1,38 @@
 import React from "react";
-import {createRoot} from "react-dom/client";
+import { createRoot } from "react-dom/client";
 import { Provider } from "react-redux";
 import { store } from "./app/store";
 import App from "./app/App";
 import reportWebVitals from "./reportWebVitals";
-import  CssBaseline  from "@mui/material/CssBaseline";
+import CssBaseline from "@mui/material/CssBaseline";
 import { ThemeProvider } from "@mui/material/styles";
 import theme from "./app/material/MaterialTheme";
 import { BrowserRouter as Router } from "react-router-dom";
-import "./css/index.css";
 import ContextProvider from "./app/context/ContextProvider";
+import { SocketProvider } from "./app/context/SocketContext";
+import "./css/index.css";
 
 const container = document.getElementById('root')!; //document is REAL DOM's instance, and we are getting "root" from it's method
 const root = createRoot(container);
 
 
 root.render(  //.render() → actually draws your React components inside that root.
-  <React.StrictMode> 
+  <React.StrictMode>
     {/* React’s "debugging assistant */}
     <Provider store={store}>
       {/* connecting your app to Redux(ma'lumotlar bunkeri) */}
       <ContextProvider>
-         <ThemeProvider theme={theme}>
-        {/* customized component */}
-         <CssBaseline />
-         {/* It helps ensure your app looks the same across all browsers */}
-          <Router>
-            {/* It enables routing — so you can navigate between pages (URLs) in your app without reloading. */}
-            <App />
-          </Router>
-      </ThemeProvider>
+        <SocketProvider>
+          <ThemeProvider theme={theme}>
+            {/* customized component */}
+            <CssBaseline />
+            {/* It helps ensure your app looks the same across all browsers */}
+            <Router>
+              {/* It enables routing — so you can navigate between pages (URLs) in your app without reloading. */}
+              <App />
+            </Router>
+          </ThemeProvider>
+        </SocketProvider>
       </ContextProvider>
     </Provider>
   </React.StrictMode>
